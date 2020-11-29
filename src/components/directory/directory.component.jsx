2fbaaa863fcebda1c613fb284843d2e9e1554ab1
Pursuit <rahin6810@gmail.com>
2020-11-29 05:03:08 -0500

    deleted duplicate key size from inventory

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -31,7 +31,6 @@ class Directory extends React.Component {
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           size: "large",
           id: 4,
-          size: "large",
           linkUrl: "shop/womens",
         },
         {
@@ -39,7 +38,6 @@ class Directory extends React.Component {
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
           id: 5,
-          size: "large",
           linkUrl: "shop/mens",
         },
       ],
